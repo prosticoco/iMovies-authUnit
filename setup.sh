e794ceab0348665be6b5196788e34aafd939ceac
aprost <aprost@student.ethz.ch>
2019-11-06 13:48:54 +0100

    fix

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 -m venv venv
-. venv/bin/activate
-#pip install virtualenv
-#virtualenv auth_env
-#auth_env/bin/activate
--- src/db_utils.py
+++ src/db_utils.py
@@ -5,26 +5,59 @@ import traceback
 
 
 
-
 class DBManager :
 
+	"""Summary
 
+	Baseline class to connect to a mySQL database and execute 
+	SQL queries
+	
+	Attributes:
+	    connection (TYPE): connection to the DB
+	    cursor (TYPE): DB Cursor to execute queries
+	"""
+	
 	def __init__(self,host,user,passwd,database):
-
+		"""Summary
+		
+		Args:
+		    host (TYPE): IP address of user
+		    user (TYPE): username
+		    passwd (TYPE): password
+		    database (TYPE): database name
+		"""
 		self.connection = mysql.connector.connect(host= host,user = user,
 			passwd =passwd,database = database)
 		self.cursor = self.connection.cursor(prepared=True)
 
 
 	def err_handler(self,exc):
-
+		"""Summary
+		
+		Args:
+		    exc (Exception, optional): Exception to print
+		"""
 		print("DBManager Error :")
 		print(type(exc))
 		traceback.print_exc()
 
 
 	def query_db(self,query,q_tuple=None,write=False,prop=False):
-
+		"""Main method to query the database supports any kind of
+		queries. Need to indicate is of type WRITE 
+		
+		Args:
+		    query (TYPE): query parametrized or not
+		    q_tuple (None, optional): tuple if query is parametrized
+		    write (bool, optional): Indicates if DB is being updated
+		    prop (bool, optional): Indicates if DB errors should be propagated
+		
+		Returns:
+		    TYPE: Results from the query if READ query 
+		
+		Raises:
+		    Exception: Exception to be propagated
+		"""
 		result = None
 
 		try:
@@ -37,6 +70,7 @@ class DBManager :
 
 				self.cursor.execute(query)
 			
+
 			if write :
 
 				self.connection.commit()
@@ -45,14 +79,16 @@ class DBManager :
 
 				result = self.cursor.fetchall()
 
+
 		except Exception as e :
 
 			self.err_handler(e)
 
 			if prop :
 
-				raise Exception
+				raise e
 
+				
 		return result
 
 
@@ -62,9 +98,24 @@ class DBManager :
 
 if __name__ == '__main__':
 	
-	example_query_select = """INSERT INTO users VALUES ('id2',
-	'Smith','Will','ws@aol.com','1234notevenhashed')"""
-	test = DBManager('localhost','root','toor','imovies')
-	test.query_db(example_query_select)
+	example_query_select = """SELECT * FROM users"""
+
+	example_query_prepared = """SELECT * FROM users
+	 WHERE lastname = %s"""
+	tuple_prepared = 'Schaller',
+
+
+	example_query_insert = """INSERT INTO users VALUES (%s,%s,%s,%s,%s) """
+	values_tuple = 'id42','Smith','John','johns@hotmail.ch','thispwdshouldbehashed'
+
+	db_manager = DBManager('localhost','root','toor','imovies')
+	result = db_manager.query_db(example_query_select)
+	print(result)
+	result = db_manager.query_db(example_query_prepared,tuple_prepared)
+	print(result)
+	result = db_manager.query_db(example_query_insert,values_tuple,write=True)
+	print(result)
+	result = db_manager.query_db(example_query_select)
+	print(result)
 
 
