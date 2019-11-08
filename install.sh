fffca0c06fe20f48045dfc92ca581bec5d3a2f51
aprost <aprost@student.ethz.ch>
2019-11-08 17:43:23 +0100

    fix script

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 DB_ROOT_PASSWORD=$1
 DUMP_PATH=$2
 
+
+apt-get update
+apt-get upgrade
+apt-get install python-venv
 sh scripts/install_mysql.sh $DB_ROOT_PASSWORD 
 sh scripts/setup_mysql.sh $DB_ROOT_PASSWORD $DUMP_PATH
 
