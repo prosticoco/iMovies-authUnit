3407279282a6ad7ddb7f2146b68935fe15499904
aprost <aprost@student.ethz.ch>
2019-11-09 12:51:29 +0100

    script for env

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ DUMP_PATH=$2
 
 #apt-get update
 #apt-get upgrade
-chmod scripts/virtualenvsetup.sh 700
+chmod scripts/virtualenvsetup.sh u+x
 
+echo 'installing mySQL server'
 sh scripts/install_mysql.sh $DB_ROOT_PASSWORD 
+echo 'Setting up database'
 sh scripts/setup_mysql.sh $DB_ROOT_PASSWORD $DUMP_PATH
 
 ./scripts/virtualenvsetup
--- scripts/virtualenvsetup.sh
+++ scripts/virtualenvsetup.sh
@@ -1,10 +1,12 @@
 echo 'installing virtual environment and python dependencies...'
+echo 'installing venv'
 apt-get --assume-yes install python3-venv
 python3 -m venv venv
+echo 'activating environment'
 source venv/bin/activate
-
+echo 'installing requirements'
 pip install -r requirements.txt
 
-chmod run +x
+chmod u+x run
 
 . run
