ad1aba25fb73b5a3f48058b5b19ea49205d28fe1
aprost <aprost@student.ethz.ch>
2019-11-11 15:03:34 +0100

    debug

--- scripts/setup_mysql.sh
+++ scripts/setup_mysql.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 MYSQL_ROOT_PASSWORD=$1
 PATH_TO_DUMP=$2
+echo 'PATH TO DUMP'
+echo $PATH_TO_DUMP
 echo 'create database imovies' | mysql -uroot -p$MYSQL_ROOT_PASSWORD
 mysql -uroot -p$MYSQL_ROOT_PASSWORD imovies < $PATH_TO_DUMP
